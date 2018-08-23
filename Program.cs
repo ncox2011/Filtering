a0b74999eb981b75086d69f7418a17b10d84d0b5
Natasha Cox <natashac1014@gmail.com>
2018-08-23 11:36:50 -0500

    second part

--- Program.cs
+++ Program.cs
@@ -10,13 +10,26 @@ public class Filtering
         List<string> Fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
         List<string> LFruits = (from fruit in Fruits
-                                      where fruit.StartsWith("L")
-                                      select fruit).ToList();
+                                where fruit.StartsWith("L")
+                                select fruit).ToList();
 
-        
+
         foreach (string f in LFruits)
         {
             Console.WriteLine(f);
+        }
+
+
+        // Which of the following numbers are multiples of 4 or 6
+        List<int> numbers = new List<int>()
+{
+    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+};
+
+        List<int> fourSixMultiples = numbers.Where(n => (n % 4 == 0) || (n % 6 ==0)).ToList();
+
+        foreach (int n in fourSixMultiples) {
+            Console.WriteLine(n);
         }
     }
 }
