b60ad06b7ce20ee410d6256fe0af59d603e6ecf6
Natasha Cox <natashac1014@gmail.com>
2018-08-23 11:28:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Filtering
+{
+    public static void Main()
+    {
+        // Find the words in the collection that start with the letter 'L'
+        List<string> Fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+        List<string> LFruits = (from fruit in Fruits
+                                      where fruit.StartsWith("L")
+                                      select fruit).ToList();
+
+        
+        foreach (string f in LFruits)
+        {
+            Console.WriteLine(f);
+        }
+    }
+}
